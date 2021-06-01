088c46f9ca8f79ac5a235a6040bac1678e1a2267
dgtechboost <25640061+dgtechboost@users.noreply.github.com>
2021-06-01 14:48:10 +0100

    package json add refresh token command, up version of the npm package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AzureNpmNgLibService } from 'azure-npm-ng-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AzureArtifactsNpmConsumer';
+
+  constructor(private _azureNpmNgLibService: AzureNpmNgLibService) {
+  }
+
+  ngOnInit(): void {
+    this.mainFunc();
+  }
+
+  mainFunc(){
+    let message = this._azureNpmNgLibService.getMessage();
+    console.log(message);
+    console.log(123)
+  }
 }
